075ee7f2ba1c493c2355d4812218505372758859
sigurbergurm <69972318+sigurbergurm@users.noreply.github.com>
2021-04-29 14:44:10 +0000

    Saving

--- src/ConvertHexDataToDecimal.py
+++ src/ConvertHexDataToDecimal.py
@@ -4,14 +4,15 @@
 import numpy as np
 
 #Reading vals from original file
-with open('C:\\Users\\Sigur\\Documents\\Háskóoli 2021 vor\\data\\IsrLoggerTest.txt','rb') as f:
+with open(file_location,'rb') as f:
 	#Converting to decimal values
 	decimalVals = np.fromfile(f, dtype=np.uint16)
 
 #Writing to a new files
 #outFile = open("direct_long_12bit_bus_56MBs.txt",'w')
 
-outFile = open("test.txt",'w')
+outFile = open(out_file_location,'w')
+
 for i in range(0, len(decimalVals)):
 	outFile.write(str(decimalVals[i]) + '\n')
 outFile.close()
